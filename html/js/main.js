1f3c2d360ceaff6608f5368067a2363ad101fd87
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-07-29 11:43:59 +0100

    Handle redirect with fragment parameter

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+    var vars = $.deparam.fragment();;
+    var indx = vars.indx;
+    if (indx != undefined){
+        $("#server-text").html("Authorisation is for use by the INDX server: <tt>" + unescape(indx) + "</tt>, you will be redirected back there once authorisation has completed.");
+        var url = $("#server-link").attr("href");
+        url += "&state=" + indx; // don't unescape, it stays urlencoded for the link here (above is for human-readable, so we unescape it)
+        $("#server-link").attr("href", url);
+    }
+});
