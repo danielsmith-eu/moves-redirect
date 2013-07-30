c6377652366a5168353a367cf6277d967ace10aa
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-07-30 20:18:14 +0100

    CURL to POST

--- html/get_token/index.php
+++ html/get_token/index.php
@@ -11,7 +11,7 @@ $ch = curl_init($url);
  
 curl_setopt($ch, CURLOPT_POST, 1);
 #curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
  
 $response = curl_exec($ch);
 curl_close($ch);
