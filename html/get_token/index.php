39f484e5b7600e50b7042d02e019495a5aed260b
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-07-30 20:15:06 +0100

    POST request to get token

--- html/get_token/index.php
+++ html/get_token/index.php
@@ -7,5 +7,15 @@ $redirect_url = $_POST["redirect_url"];
 
 $url = "https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=".$code."&client_id=".$client_id."&client_secret=".$client_secret."&redirect_uri=".$redirect_url;
 
-print file_get_contents($url);
+$ch = curl_init($url);
+ 
+curl_setopt($ch, CURLOPT_POST, 1);
+#curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+ 
+$response = curl_exec($ch);
+curl_close($ch);
+
+print $response;
+
 ?>
